715a724b948687c0610fd6bf0178ecb806d266b2
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-11-10 23:30:22 +0530

    add functions and test function for add

---
+++ test_functions.py
@@ -0,0 +1,6 @@
+from functions import add, subtract, multiply
+from functions import convert_fahrenheit_to_celsius as f2c
+
+def test_add():
+    assert add(2, 3) == 5
+    assert add('space', 'ship') == 'spaceship'
