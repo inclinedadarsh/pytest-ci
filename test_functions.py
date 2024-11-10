1346ffcfa2eca130856cb2c5fa23a26fd8a5e183
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-11-11 00:00:59 +0530

    add test which fails

--- test_functions.py
+++ test_functions.py
@@ -4,3 +4,6 @@ from functions import convert_fahrenheit_to_celsius as f2c
 def test_add():
     assert add(2, 3) == 5
     assert add('space', 'ship') == 'spaceship'
+
+def test_subtract():
+    assert subtract(2, 3) == -1
