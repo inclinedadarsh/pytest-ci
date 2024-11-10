9358da694d453dd516b15834fe6f0333b72bf5fc
Adarsh Dubey <84132532+inclinedadarsh@users.noreply.github.com>
2024-11-11 00:18:06 +0530

    add test coverage for full code (#3)

--- test_functions.py
+++ test_functions.py
@@ -1,3 +1,4 @@
+from pytest import approx
 from functions import add, subtract, multiply
 from functions import convert_fahrenheit_to_celsius as f2c
 
@@ -7,3 +8,10 @@ def test_add():
 
 def test_subtract():
     assert subtract(2, 3) == -1
+
+def test_multiply():
+    assert multiply(2, 3) == 6
+
+def test_f2c():
+    assert f2c(32) == 0
+    assert f2c(100) == approx(37.7778)
